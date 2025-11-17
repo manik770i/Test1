e5eb2259f9489118dcf87e34b371ab82d501dc14
manik770i <manik770i@gmail.com>
2025-11-18 01:02:46 +0530

    Update Newproj.spec.ts

--- tests/Newproj.spec.ts
+++ tests/Newproj.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
-    await page.goto('https://exampleoneTwo.com');
+    await page.goto('https://example1.com');
     await expect(page).toHaveTitle(/Example Domain/);
 });
